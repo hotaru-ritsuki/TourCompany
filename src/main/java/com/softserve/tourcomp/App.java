b4f5e7b704dbe98a19e228fa902cb8285427c4bf
Vasyl_Petrashchuk <rockeman2013@gmail.com>
2019-12-11 12:09:54 +0200

    JSP PAGES

--- src/main/java/com/softserve/tourcomp/App.java
+++ src/main/java/com/softserve/tourcomp/App.java
@@ -16,7 +16,9 @@ import java.sql.SQLException;
  */
 public class App {
   public static void main(String[] args) throws SQLException {
-
+ServiceFactory sf =ServiceFactory.getInstance();
+UserService us=sf.getUserService();
+    System.out.println(us.valid("asasfsfsdf@gmail.com", "daosfksdf"));
 
   }
 }
